08b64977299819094931400fabefe56b365db194
bjornwann <bjorn.wann@chasacademy.se>
2021-03-17 11:38:22 +0100

    end of page 47

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-
-
 const App = () => {
   const stories = [
     {
@@ -26,38 +24,33 @@ const App = () => {
       points: 400,
       objectID: 2,
     }];
-  // A
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+  
   const handleSearch = event => {
-    // C
-    console.log(event.target.value);
+   setSearchTerm(event.target.value);
   };
 
+  const searchedStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search onSearch={handleSearch} />
 
-
       <hr />
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>);
 }
 
 const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
-    // B
-    props.onSearch(event);
-  };
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-</p> </div>
+      <input id="search" type="text" onChange={props.onSearch} />
+     </div>
   );
 };
 
