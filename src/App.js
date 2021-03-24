27e96bc9a7cc1df0a0888602b5bb82d4d4379147
bjornwann <bjorn.wann@chasacademy.se>
2021-03-24 21:45:55 +0100

    end of page 83

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const useSemiPersistentState = (key, initialState) => {
 
 const App = () => {
 
-  const stories = [
+  const initialStories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
@@ -40,6 +40,16 @@ const App = () => {
     }];
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'Redux');
+  
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = item => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+    
+    setStories(newStories);
+  };
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
@@ -57,29 +67,48 @@ const App = () => {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <InputWithLabel id='search' value={searchTerm} onInputChange={handleSearch}>
+      <InputWithLabel 
+        id='search' 
+        value={searchTerm} 
+        isFocused 
+        onInputChange={handleSearch}
+      >
         <SimpleText><div>Search:</div></SimpleText>
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>);
 }
 
 
-const InputWithLabel = ({ children, id, value, type = 'text', onInputChange }) => (
-  <>
-    <label htmlFor={id}>{children}</label> &nbsp;
-    <input
-      id={id}
-      type={type} value={value} onChange={onInputChange}
-    />
-  </>
-);
+const InputWithLabel = ({ children, id, value, isFocused, type = 'text', onInputChange }) => {
+  const inputRef = React.useRef();
 
-const List = ({ list }) =>
-  list.map(item => <Item key={item.objectID} item={item} />);
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
 
-const Item = ({ item }) => (
+  return (
+    <>
+      <label htmlFor={id}>{children}</label> &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type} 
+        value={value}
+        autoFocus={isFocused} 
+        onChange={onInputChange}
+      />
+    </>
+  )
+};
+
+const List = ({ list, onRemoveItem }) =>
+  list.map(item => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />);
+
+const Item = ({ item, onRemoveItem }) => (
   <div>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -87,6 +116,11 @@ const Item = ({ item }) => (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
   </div>
 );
 
