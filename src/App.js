99d8143cd72825d81986a9fef8f3aa2a10935ccf
bjornwann <bjorn.wann@chasacademy.se>
2021-04-01 01:35:57 +0200

    end of page 143

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -46,6 +47,81 @@ const storiesReducer = (state, action) => {
   }
 };
 
+const StyledContainer = styled.div`
+  height: 100vw;
+  padding: 20px;
+  background: #83a4d4;
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+  color: #171212;
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weight: 300;
+  letter-spacing: 2px;
+`;
+
+const StyledItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  
+  a {
+    color: inherit;
+  }
+  
+  width: ${props => props.width};
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+  transition: all 0.1s ease-in;
+  
+  &:hover {
+    background: #171212;
+    color: #ffffff;
+  }
+`;
+
+const StyledButtonSmall = styled(StyledButton)`
+  padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20px 0;
+  display: flex;
+  align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-left: 1px solid #171212;
+  padding-left: 5px;
+  font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  border-bottom: 1px solid #171212;
+  background-color: transparent;
+  font-size: 24px;
+`;
+
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const App = () => {
@@ -89,9 +165,10 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = event => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
-  };
+    event.preventDefault();
+  }
 
   // Create a simple text component that renders a string and passes it as children to the
   // InputWithLabel component.
@@ -100,27 +177,36 @@ const App = () => {
     <strong>{children}</strong>
   );
 
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <InputWithLabel 
-        id='search' 
-        value={searchTerm} 
-        isFocused 
-        onInputChange={handleSearchInput}
+  const SearchForm = ({
+      searchTerm,
+      onSearchInput,
+      onSearchSubmit,
+    }) => (
+    <StyledSearchForm onSubmit={onSearchSubmit}>
+      <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
       >
         <SimpleText><div>Search:</div></SimpleText>
       </InputWithLabel>
+     
+      <StyledButtonLarge type="submit" disabled={!searchTerm}>
+      Submit
+      </StyledButtonLarge>
+    </StyledSearchForm>
+  );
 
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
+  return (
+    <StyledContainer>
+      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
       
-      <hr />
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
       
       {stories.isError && <p>Something went wrong ...</p>}
       
@@ -132,7 +218,7 @@ const App = () => {
           onRemoveItem={handleRemoveStory}
         />
       )}
-    </div>
+    </StyledContainer>
   );
 }
 
@@ -147,14 +233,13 @@ const InputWithLabel = ({ children, id, value, isFocused, type = 'text', onInput
 
   return (
     <>
-      <label htmlFor={id}>{children}</label> &nbsp;
-      <input
+      <StyledLabel htmlFor={id}>{children}</StyledLabel> &nbsp;
+      <StyledInput
         ref={inputRef}
-        id={id}
-        type={type} 
-        value={value}
-        autoFocus={isFocused} 
-        onChange={onInputChange}
+          id={id}
+          type={type}
+          value={value}
+          onChange={onInputChange}
       />
     </>
   )
@@ -164,19 +249,19 @@ const List = ({ list, onRemoveItem }) =>
   list.map(item => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />);
 
 const Item = ({ item, onRemoveItem }) => (
-  <div>
-    <span>
+  <StyledItem>
+    <StyledColumn width="40%">
       <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={() => onRemoveItem(item)}>
+    </StyledColumn>
+    <StyledColumn width="30%">{item.author}</StyledColumn>
+    <StyledColumn width="10%">{item.num_comments}</StyledColumn>
+    <StyledColumn width="10%">{item.points}</StyledColumn>
+    <StyledColumn width="10%">
+      <StyledButtonSmall type="button" onClick={() => onRemoveItem(item)}>
         Dismiss
-      </button>
-    </span>
-  </div>
+      </StyledButtonSmall>
+    </StyledColumn>
+  </StyledItem>
 );
 
 export default App;
